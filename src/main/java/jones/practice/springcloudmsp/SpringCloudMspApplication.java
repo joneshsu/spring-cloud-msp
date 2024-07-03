ea09db83bc4565c1d7201c03dde2f77bafaa03d4
Jones Hsu <jones.th.hsu@gmail.com>
2024-07-03 08:35:09 +0800

    initial project

---
+++ src/main/java/jones/practice/springcloudmsp/SpringCloudMspApplication.java
@@ -0,0 +1,13 @@
+package jones.practice.springcloudmsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jones/practice/springcloudmsp/SpringCloudMspApplicationTests.java
@@ -0,0 +1,13 @@
+package jones.practice.springcloudmsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
